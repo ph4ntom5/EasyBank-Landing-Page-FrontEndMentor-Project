ebb3e33b8fdd00eac15932bf45d2412d2940eba1
ph4ntom5 <tomsstals1@gmail.com>
2021-04-20 14:38:03 +0200

    wip

--- easybank-lp/src/components/Hero.js
+++ easybank-lp/src/components/Hero.js
@@ -1,9 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "../components/Hero.scss";
+import mockupImage from "../images/bg-intro-mobile.svg";
+import phoneMockups from "../images/image-mockups.png";
 
 const Hero = () => (
   <>
-    <div className="Hero-img"></div>
+    <div className="Hero-img">
+      <img
+        className="Bg-blobs"
+        src={mockupImage}
+        alt="Background green and violet blob shapes"
+      ></img>
+      <img
+        className="Mobile-phones"
+        src={phoneMockups}
+        alt="Mobile Phones showing a software app"
+      ></img>
+    </div>
     <div className="Headline">
       <h1>Next generation digital banking</h1>
       <p>
