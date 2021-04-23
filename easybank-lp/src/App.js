a349529fae87a918b5ae4e15224cad3c219c1c83
ph4ntom5 <tomsstals1@gmail.com>
2021-04-23 15:48:53 +0200

    wip

--- easybank-lp/src/App.js
+++ easybank-lp/src/App.js
@@ -1,12 +1,14 @@
 import "./App.scss";
 import Hero from "./components/Hero.js";
 import Features from "./components/Features.js";
+import Footer from "./components/Footer.js";
 
 function App() {
   return (
     <div className="App">
       <Hero />
       <Features />
+      <Footer />
     </div>
   );
 }
---
+++ easybank-lp/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "../components/Footer.scss";
+
+const Footer = () => (
+  <>
+    <div className="Footer-section"></div>
+  </>
+);
+
+export default Footer;
